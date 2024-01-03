79fd1758dc1844c5695b497e97e31de32089a6e2
crisavendano <coavendano@uc.cl>
2024-01-03 14:06:00 -0300

    test: added GameOverTests

---
+++ Minesweeper/Minesweeper.Tests/GameOverTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Minesweeper.Tests;
+
+public class GameOverTests
+{
+    [Theory]
+    [InlineData(0,0, true)]
+    public void Should_EndGame(int row, int column, bool hasToBeRevealed)
+    {
+        Board board = BoardFactory.CreateBoard(10, 10, 10);
+        if (hasToBeRevealed) board.GetTiles()[row, column].Reveal();
+        bool isBomb = board.GetTiles()[row, column].GetType() == TileType.Bomb;
+        bool isRevealed = board.GetTiles()[row, column].IsRevealed();
+        bool hasLost = isBomb && isRevealed;
+        Assert.Equal(hasLost, GameOverChecker.CheckIfLost(board));
+    }
+    
+}
